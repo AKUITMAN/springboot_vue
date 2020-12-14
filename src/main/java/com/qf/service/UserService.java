98914c185be16074f7d61254e856e06134ea1457
AKUITLI <2438435581@qq.com>
2020-12-14 08:52:52 +0800

    这是登陆注册的上传

--- src/main/java/com/qf/service/UserService.java
+++ src/main/java/com/qf/service/UserService.java
@@ -1,7 +1,13 @@
 package com.qf.service;
 
 import com.qf.commom.BaseResponse;
+import com.qf.pojo.User;
+import com.qf.pojo.req.UserReq;
 
 public interface UserService {
     BaseResponse sendMail(String email);
+
+    BaseResponse registry(UserReq userReq);
+
+    BaseResponse login(UserReq user);
 }
